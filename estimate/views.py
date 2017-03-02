5e51754903d72eb3eb7b38cf9e1d80c49cb353e0
Westporch <westporch@gmail.com>
2017-03-02 19:35:03 +0900

    작업 중

--- estimate/views.py
+++ estimate/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import get_object_or_404, render
 from estimate.models import Product
 
+#def index(request):
+#    product_list = Product.objects.all()
+#    context = {'product_list': product_list}
+#    return render(request, 'estimate/index.html', context)
+
 def index(request):
     product_list = Product.objects.all()
-    context = {'product_list': product_list}
+
+    price = 1000000
+    #context = {'product_list': product_list, 'test_price': price, 'get_price': Product.objects.values('unit_price')}
+    context = {'product_list': product_list, 'test_price': price, 'get_price': Product.objects.values('unit_price')}
+    #context = {'product_list': product_list, 'test_price': price, 'get_price': Product.objects.filter(item_name='U+XEN')}
+
     return render(request, 'estimate/index.html', context)
--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/root/git/estimate/myenv"
+set -gx VIRTUAL_ENV "/home/westporch/Git/estimate/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
